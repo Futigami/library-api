599a6c939769c3be604c873dde06fa7ce452685c
Futigami <erick.ertuito@hotmail.com>
2020-08-10 13:26:56 -0300

    Aplicando livro em uma base de dados

--- src/test/java/com/futigami/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/com/futigami/libraryapi/model/repository/BookRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.futigami.libraryapi.model.repository;
 
+import com.futigami.libraryapi.model.entity.Book;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,9 +27,25 @@ public class BookRepositoryTest {
     @DisplayName("Retorna verdadeiro quando existir um livro na base e um isbn válido")
     public void returnTrueWhenIsbnExists(){
         String isbn = "123";
+        Book book = Book.builder()
+                .title("Aventuras")
+                .author("Fulano")
+                .isbn(isbn)
+                .build();
+        entityManager.persist(book);
 
         boolean exists = repository.existsByIsbn(isbn);
 
         assertThat(exists).isTrue();
+    }
+
+    @Test
+    @DisplayName("Retorna false quando não existir um livro na base e um isbn válido")
+    public void returnFalseWhenIsbnDoesntExists(){
+        String isbn = "123";
+
+        boolean exists = repository.existsByIsbn(isbn);
+
+        assertThat(exists).isFalse();
     }
 }
