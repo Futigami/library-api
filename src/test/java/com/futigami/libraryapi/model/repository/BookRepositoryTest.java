b29b90a0ecf28f01bbe22d096a3a29bb8337c854
Futigami <erick.ertuito@hotmail.com>
2020-08-13 14:23:28 -0300

    Teste de integração findById

--- src/test/java/com/futigami/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/com/futigami/libraryapi/model/repository/BookRepositoryTest.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -27,17 +29,15 @@ public class BookRepositoryTest {
     @DisplayName("Retorna verdadeiro quando existir um livro na base e um isbn válido")
     public void returnTrueWhenIsbnExists(){
         String isbn = "123";
-        Book book = Book.builder()
-                .title("Aventuras")
-                .author("Fulano")
-                .isbn(isbn)
-                .build();
+        Book book = createNewBook(isbn);
         entityManager.persist(book);
 
         boolean exists = repository.existsByIsbn(isbn);
 
         assertThat(exists).isTrue();
     }
+
+
 
     @Test
     @DisplayName("Retorna false quando não existir um livro na base e um isbn válido")
@@ -47,5 +47,23 @@ public class BookRepositoryTest {
         boolean exists = repository.existsByIsbn(isbn);
 
         assertThat(exists).isFalse();
+    }
+
+    @Test
+    @DisplayName("Deve obter um livro por id")
+    public void findByIdTest(){
+        Book book = createNewBook("123");
+        entityManager.persist(book);
+
+        Optional<Book> foundBook = repository.findById(book.getId());
+
+        assertThat(foundBook.isPresent()).isTrue();
+    }
+
+    private Book createNewBook(String isbn) {
+        return Book.builder()
+                .title("Aventuras")
+                .author("Fulano")
+                .isbn(isbn).build();
     }
 }
