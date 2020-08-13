8d639ccfea47206c93479f0b9a94e4bc7b600502
Futigami <erick.ertuito@hotmail.com>
2020-08-13 14:15:22 -0300

    Teste unitário na camada de serviço

--- src/test/java/com/futigami/libraryapi/service/BookServiceTest.java
+++ src/test/java/com/futigami/libraryapi/service/BookServiceTest.java
@@ -15,6 +15,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -66,6 +68,40 @@ public class BookServiceTest {
         assertThat(exception).isInstanceOf(BusinessException.class)
                 .hasMessage("Isbn já cadastrado.");
         Mockito.verify(repository, Mockito.never()).save(book);
+    }
+
+    @Test
+    @DisplayName("Obtem um livro por Id")
+    public void getByIdTest(){
+        Long id = 1l;
+
+        Book book = createValidBook();
+        book.setId(id);
+        Mockito.when(repository.findById(id))
+                .thenReturn(Optional.of(book));
+
+        Optional<Book> foundBook = service.getById(id);
+
+        assertThat(foundBook.isPresent()).isTrue();
+        assertThat(foundBook.get().getId()).isEqualTo(id);
+        assertThat(foundBook.get().getAuthor()).isEqualTo(book.getAuthor());
+        assertThat(foundBook.get().getTitle()).isEqualTo(book.getTitle());
+        assertThat(foundBook.get().getIsbn()).isEqualTo(book.getIsbn());
+
+    }
+
+    @Test
+    @DisplayName("Retornar vazio ao obter um livro por Id quando ele nãi existe na base de dados")
+    public void bookNotFoundByIdTest(){
+        Long id = 1l;
+
+        Mockito.when(repository.findById(id))
+                .thenReturn(Optional.empty());
+
+        Optional<Book> foundBook = service.getById(id);
+
+        assertThat(foundBook.isPresent()).isFalse();
+
     }
 
     private Book createValidBook() {
